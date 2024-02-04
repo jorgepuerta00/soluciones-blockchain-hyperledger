fd9d065ad4695cf897cfb1c2042c4c3f2b926aa4
Jorge Puerta <jorge.puerta00@gmail.com>
2024-02-04 19:37:41 +0100

    added scripts

---
+++ universidades/scripts/channel_list.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Define an array of peer configurations
+declare -A peers
+peers=(
+    ["peer0.madrid.universidades.com:7051"]="MadridMSP|${PWD}/organizations/peerOrganizations/madrid.universidades.com/peers/peer0.madrid.universidades.com/tls/ca.crt|${PWD}/organizations/peerOrganizations/madrid.universidades.com/users/Admin@madrid.universidades.com/msp"
+    ["peer0.bogota.universidades.com:9051"]="BogotaMSP|${PWD}/organizations/peerOrganizations/bogota.universidades.com/peers/peer0.bogota.universidades.com/tls/ca.crt|${PWD}/organizations/peerOrganizations/bogota.universidades.com/users/Admin@bogota.universidades.com/msp"
+    ["peer0.berlin.universidades.com:9051"]="BerlinMSP|${PWD}/organizations/peerOrganizations/berlin.universidades.com/peers/peer0.berlin.universidades.com/tls/ca.crt|${PWD}/organizations/peerOrganizations/berlin.universidades.com/users/Admin@berlin.universidades.com/msp"
+)
+
+# Loop through the array and set environment variables for each peer
+for peer_address in "${!peers[@]}"; do
+    IFS='|' read -ra ADDR <<< "${peers[$peer_address]}"
+    export CORE_PEER_LOCALMSPID="${ADDR[0]}"
+    export CORE_PEER_TLS_ROOTCERT_FILE="${ADDR[1]}"
+    export CORE_PEER_MSPCONFIGPATH="${ADDR[2]}"
+    export CORE_PEER_ADDRESS="$peer_address"
+
+    echo "Listing channels for peer: $CORE_PEER_ADDRESS"
+    peer channel list
+    echo "---------------------------------------------"
+done
---
+++ universidades/scripts/join_channel.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Definir la ruta al bloque de génesis del canal
+CHANNEL_BLOCK_PATH="./channel-artifacts/universidadeschannel.block"
+CHANNEL_NAME="universidadeschannel"
+
+# Configuración para el orderer
+export ORDERER_CA="${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/msp/tlscacerts/tlsca.universidades.com-cert.pem"
+export ORDERER_ADMIN_TLS_SIGN_CERT="${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/tls/server.crt"
+export ORDERER_ADMIN_TLS_PRIVATE_KEY="${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/tls/server.key"
+
+# Unir el orderer al canal
+echo "Uniendo el orderer al canal ${CHANNEL_NAME}..."
+osnadmin channel join --channelID $CHANNEL_NAME --config-block $CHANNEL_BLOCK_PATH -o localhost:7053 --ca-file "$ORDERER_CA" --client-cert "$ORDERER_ADMIN_TLS_SIGN_CERT" --client-key "$ORDERER_ADMIN_TLS_PRIVATE_KEY"
+echo "Orderer unido al canal."
+echo "---------------------------------------------"
+
+# Configuración para los peers
+declare -A peers
+peers=(
+    ["peer0.madrid.universidades.com:7051"]="MadridMSP|${PWD}/organizations/peerOrganizations/madrid.universidades.com/peers/peer0.madrid.universidades.com/tls/ca.crt|${PWD}/organizations/peerOrganizations/madrid.universidades.com/users/Admin@madrid.universidades.com/msp"
+    ["peer0.bogota.universidades.com:9051"]="BogotaMSP|${PWD}/organizations/peerOrganizations/bogota.universidades.com/peers/peer0.bogota.universidades.com/tls/ca.crt|${PWD}/organizations/peerOrganizations/bogota.universidades.com/users/Admin@bogota.universidades.com/msp"
+    ["peer0.berlin.universidades.com:2051"]="BerlinMSP|${PWD}/organizations/peerOrganizations/berlin.universidades.com/peers/peer0.berlin.universidades.com/tls/ca.crt|${PWD}/organizations/peerOrganizations/berlin.universidades.com/users/Admin@berlin.universidades.com/msp"
+)
+
+# Habilitar TLS si es necesario
+export CORE_PEER_TLS_ENABLED=true
+
+# Unir cada peer al canal
+for peer_address in "${!peers[@]}"; do
+    IFS='|' read -ra ADDR <<< "${peers[$peer_address]}"
+    export CORE_PEER_LOCALMSPID="${ADDR[0]}"
+    export CORE_PEER_TLS_ROOTCERT_FILE="${ADDR[1]}"
+    export CORE_PEER_MSPCONFIGPATH="${ADDR[2]}"
+    export CORE_PEER_ADDRESS="$peer_address"
+
+    echo "Uniendo el peer $CORE_PEER_ADDRESS al canal ${CHANNEL_NAME}..."
+    peer channel join -b $CHANNEL_BLOCK_PATH
+    echo "Peer unido al canal."
+    echo "---------------------------------------------"
+done
